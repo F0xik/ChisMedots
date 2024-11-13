45048dfdc8d98bf8e9973dbd2f6f037ba8e922dc
F0xik <tanasov0672@mail.ru>
2024-11-13 21:30:02 +0300

    ver 1.1

--- ChisMedots/Program.cs
+++ ChisMedots/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MathNet.Symbolics;
+using System;
+using Expr = MathNet.Symbolics.SymbolicExpression;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Globalization;
@@ -10,119 +12,157 @@ namespace ChisMedots
 {
     internal class Program
     {
-        static double Function(double x)
+        public static float accuracy = 0, x0 = 0, x = 0, a = 0, b = 0, x1 = 0, x2 = 0, min = 0;
+        public static int methods;
+        //static float Function(float x)
+        //{
+        //    return (float)(3 * Math.Sin(x) - x + 3);
+        //}
+
+        static float Function(float x)
         {
-            return 3 * Math.Sin(x) - x + 3;
+            return (float)(Math.Log(x) + x - 2);
         }
+
+        static float Derivative(float x, float accuracy)
+        {
+            float h = accuracy; // Шаг для численного дифференцирования
+            return (Function(x + h) - Function(x - h)) / (2 * h);
+        }
+
+
+        //static float Function(float x)
+        //{
+        //    return (float)Math.Pow(x, 2) - 2;
+        //}
+
+        ////Для итераций 1 1e-6
+        //static float Function(float x)
+        //{
+        //    return (float)(x + 2 / x) / 2;
+        //}
+
+
+
         static void Main(string[] args)
         {
-            int methods;
-            double accuracy, x0 = 0, x = 0, a = 0, b = 0, x1 = 0, x2 = 0, min = 0;
 
-
+            Console.WriteLine("f(x) = Log(x) + x - 2");
             Console.WriteLine("Выберите метод который будет использоваться");
-            Console.WriteLine("0 - Метод дихотомии\n1 - Метод итераций\n2 - Метод Касательных\n3 - Метод хорд и секущих");
+            Console.WriteLine("1 - Метод дихотомии\n2 - Метод итераций\n3 - Метод Касательных\n4 - Метод хорд");
             methods = Convert.ToInt32(Console.ReadLine());
+            inputData();
+            executingMethods();
 
+            Console.WriteLine($"x = {Math.Round(x, countNum(accuracy))}");
+
+
+
+        }
+
+        static int countNum(float accuracy)
+        {
+            string str = accuracy.ToString(new System.Globalization.NumberFormatInfo() { NumberDecimalSeparator = "," });
+            return str.Contains(",") ? str.Remove(0, Math.Truncate(accuracy).ToString().Length + 1).Length : 0;
+        }
+
+        static void inputData()
+        {
+            switch (methods)
+            {
+                case 1:
+                case 3:
+                case 4:
+                    Console.WriteLine("Введите a: ");
+                    a = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите b: ");
+                    b = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите e: ");
+                    accuracy = float.Parse(Console.ReadLine());
+                    break;
+
+                case 2:
+                    Console.WriteLine("Введите x0: ");
+                    x0 = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите e: ");
+                    accuracy = float.Parse(Console.ReadLine());
+                    break;
+            }
+        }
+
+        static void executingMethods()
+        {
 
             switch (methods)
             {
+
                 //Метод дихотомии
-                case 0:
+                case 1:
                     {
-                        Console.WriteLine("Введите погрешность: ");
-                        accuracy = Convert.ToDouble(Console.ReadLine());
-
-                        Console.WriteLine("Введите первую границу: ");
-                        a = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Введите вторую границу: ");
-                        b = Convert.ToDouble(Console.ReadLine());
-
-                        while (b - a >= 2 * accuracy)
+                        while (b - a > accuracy)
                         {
-                            x1 = (a + b - accuracy) / 2;
-                            x2 = (a + b + accuracy) / 2;
+                            x = (a + b) / 2;
 
-                            if (Function(x1) <= Function(x2))
+                            if (Function(a) * Function(x) < 0)
                             {
-                                b = x2;
+                                b = x;
                             }
                             else
                             {
-                                a = x1;
+                                a = x;
                             }
                         }
                         x = (a + b) / 2;
-                        min = Function(x);
-
-                        Console.WriteLine($"Минимум: x = {x}, f(x) = {min}");
+                        
                     }
-                break;
+                    break;
 
-                    // Метод итераций
-                case 1:
+
+                // Метод итераций
+                case 2:
                     {
-                        Console.WriteLine("Введите x0: ");
-                        x0 = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Введите погрешность: ");
-                        accuracy = Convert.ToDouble(Console.ReadLine());
                         x1 = Function(x0);
                         while (Math.Abs(x1 - x0) > accuracy)
                         {
                             x0 = x1;
                             x1 = Function(x0);
                         }
-                        x = x1;
-                        Console.WriteLine("x = " + x);
-                    }
-                    break;
-                
-                    // Метод секущих
-                case 2:
-                    {
-                        Console.WriteLine("Введите первое приближение x0: ");
-                        x0 = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Введите второе приближение x1: ");
-                        x1 = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Введите погрешность: ");
-                        accuracy = Convert.ToDouble(Console.ReadLine());
-
-                       
-
-                        while (Math.Abs(x1 - x0) > accuracy)
-                        {
-                            double fx0 = Function(x0);
-                            double fx1 = Function(x1);
-                            x2 = x1 - fx1 * (x1 - x0) / (fx1 - fx0);
-
-                            x0 = x1;
-                            x1 = x2;
-                        }
-
-                        Console.WriteLine($"Корень x = {x1}");
-                        
+                        x = x1; 
                     }
                     break;
 
-
-
-                    //Метод хорд и секущих
+                // Метод касательных
                 case 3:
                     {
-                        Console.WriteLine("Введите a: ");
-                        a = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Введите b: ");
-                        b = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Введите погрешность: ");
-                        accuracy = Convert.ToDouble(Console.ReadLine());
+                         float h = (b - a) / 100; 
+                         x = Function(a); 
+                            float y = Function(a + 2 * h) - 2 * Function(a + h) + Function(a); 
+
+                         x0 = (x * y > 0) ? a : b; 
+
+                         x1 = x0 - Function(x0) / Derivative(x0, accuracy);
+
                         
+                        while (Math.Abs(x1 - x0) > accuracy)
+                        {
+                            x0 = x1;
+                            x1 = x0 - Function(x0) / Derivative(x0, accuracy);
+                        }
+                        x = x1;
+                    }
+                    break;
 
-                        double h = (b - a) / 100;
+
+
+                //Метод хорд
+                case 4:
+                    {
+                        float h = (b - a) / 100;
                         x = a;
-                        double y = a + h;
+                        float y = a + h;
 
-                        double m = (Math.Abs(Function(y) - Function(x))) / h;
-                        double c = 0;
+                        float m = (Math.Abs(Function(y) - Function(x))) / h;
+                        float c = 0;
                         while (y < b)
                         {
                             x = y;
@@ -141,28 +181,22 @@ namespace ChisMedots
 
                         x1 = x0 - Function(x0) / m;
 
-                        int maxIterations = 10000; 
-                        int iterations = 0;
 
-                        while (Math.Abs(x1 - x0) > accuracy && iterations < maxIterations)
+
+                        while (Math.Abs(x1 - x0) > accuracy)
                         {
                             x0 = x1;
                             x1 = x0 - Function(x0) / m;
-                            iterations++;
                         }
 
                         x = x1;
-                                              
-                            Console.WriteLine("x = " + x);
+
                         
                     }
                     break;
+            
 
             }
-
-
-
-
         }
     }
 }
